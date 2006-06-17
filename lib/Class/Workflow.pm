7b7a21f971b68d9305c41e207f4be82253632383
Yuval Kogman <nothingmuch@woobling.org>
2006-06-17 22:40:13 +0000

    docs for Class::Workflow itself

--- lib/Class/Workflow.pm
+++ lib/Class/Workflow.pm
@@ -164,21 +164,32 @@ Class::Workflow - Light weight workflow system.
 
 	use Class::Workflow;
 
+	# ***** NOTE *****
+	#
+	# This is a pretty long and boring example
+	#
+	# you probably want to see some flashy flash videos, so look in SEE ALSO
+	# first ;-)
+	#
+	# ****************
+
 	# a workflow object assists you in creating state/transition objects
 	# it lets you assign symbolic names to the various objects to ease construction
 
-	# you can still create the state, transition and instance objects manually.
-
 	my $wf = Class::Workflow->new;
+
+	# ( you can still create the state, transition and instance objects manually. )
 
 
 	# create a state, and set the transitions it can perform
-	# set it as the initial state
 
 	$wf->state(
 		name => "new",
 		transitions => [qw/accept reject/],
 	);
+
+	# set it as the initial state
+
 	$wf->initial_state("new");
 
 
@@ -246,6 +257,449 @@ Class::Workflow - Light weight workflow system.
 	}
 
 =head1 DESCRIPTION
+
+Workflow systems let you build a state machine, with transitions between
+states.
+
+=head1 BUG TRACKER EXAMPLE
+
+One of the simplest examples of a workflow which you've probably used is a bug
+tracking application:
+
+=over 4
+
+The initial state is 'new'
+
+=item new
+
+New bugs arrive here.
+
+=over 4
+
+=item reject
+
+This bug is not valid.
+
+Target state: C<rejected>.
+
+=item accept
+
+This bug needs to be worked on.
+
+Target state: C<open>.
+
+=back
+
+=item rejected
+
+This is the state where deleted bugs go, it has no transitions.
+
+=item open
+
+The bug is being worked on right now.
+
+=over 4
+
+=item reassign
+
+Pass the bug to someone else.
+
+Target state: C<up_for_grabs>.
+
+=item fixed
+
+The bug looks fixed, and needs verifification.
+
+Target state: C<awaiting_approval>.
+
+=back
+
+=item up_for_grabs
+
+The bug is waiting for a developer to take it.
+
+=over 4
+
+=item take
+
+Volunteer to handle the bug.
+
+Target state: C<open>.
+
+=back
+
+=item awaiting_approval
+
+The submitter needs to verify the bug.
+
+=over 4
+
+=item resolved
+
+The bug is resolved and can be closed.
+
+Target state: C<closed>
+
+=item unresolved
+
+The bug needs more work.
+
+Target state: C<open>
+
+=item closed
+
+This is, like rejected, an end state (it has no transitions).
+gg
+=back
+
+If you read through this very simple state machine you can see that it
+describes the steps and states a bug can go through in a bug tracking system.
+The core of every workflow is a state machine.
+
+=head1 INSTANCES
+
+On the implementation side, the core idea is that every "item" in the system
+(in our example, a bug) has a workflow B<instance>. This instance represents
+the current position of the item in the workflow, along with history data (how
+did it get here).
+
+In this implementation, the instance is usually a consumer of
+L<Class::Workflow::Instance>, typically L<Class::Workflow::Instance::Simple>.
+
+So, when you write your MyBug class, it should look like this (if it were written
+in L<Moose>):
+
+	package MyBug;
+	use Moose;
+
+	has workflow_instance => (
+		does => "Class::Workflow::Instance", # or a more restrictive constraint
+		is   => "rw",
+	);
+
+Since this system is purely functional (at least if your transitions are), you
+need to always set the instance after applying a transition.
+
+For example, let's say you have a handler for the "accept" action, to change
+the instance's state it would do something like this:
+
+	sub accept {
+		my $bug = shift;
+
+		my $wi = $bug->workflow_instance;
+		my $current_state = $wi->state;
+
+		# if your state supports named transitions	
+		my $accept = $current_state->get_transition( "accept" )
+			or die "There's no 'accept' transition in the current state";
+
+		my $wi_accepted = $accept->apply( $wi );
+
+		$bug->workflow_instance( $wi_accepted );
+	}
+
+=head1 RESTRICTIONS
+
+Now let's decsribe some restrictions on this workflow.
+
+=over 4
+
+=item *
+
+Only the submitter can approve the bug as resolved.
+
+=item *
+
+Only the developer can claim the bug was fixed, and reassign the bug.
+
+=item *
+
+Any developer (but not the submitter) can accept a bug as valid, into the
+'open' state.
+
+=back
+
+A workflow system will not only help in modelying the state machine, but also
+help you create restrictions on how states need to be changed, etc.
+
+The implementation of restrictions is explained after the next section.
+
+=head1 CONTEXTS
+
+In order to implement these restrictions cleanly you normally use a context
+object (a default one is provided in L<Class::Workflow::Context> but you can
+use B<anything>).
+
+This is typically the first (and sometimes only) argument to all transition
+applications, and it describes the context that the transition is being applied
+in, that is who is applying the transition, what are they applying it with, etc
+etc.
+
+In our bug system we typically care about the user, and not much else.
+
+Imagine that we have a user class:
+
+	package MyUser;
+
+	has id => (
+		isa => "Num",
+		is  => "ro",
+		default => sub { next_unique_id() };
+	);
+
+	has name => (
+		...
+	);
+
+We can create a context like this:
+
+	package MyWorkflowContext;
+	use Moose;
+
+	extends "Class::Workflow::Context";
+
+	has user => (
+		isa => "MyUser",
+		is  => "rw",
+	);
+
+to contain the "current" user.
+
+Then, when we apply the transition a bit differently:
+
+	sub accept {
+		my ( $bug, $current_user ) = @_;
+
+		my $wi = $bug->workflow_instance;
+		my $current_state = $wi->state;
+
+		# if your state supports named transitions	
+		my $accept = $current_state->get_transition( "accept" )
+			or die "There's no 'accept' transition in the current state";
+
+		my $c = MyWorkflowContext->new( user => $current_user );
+		my $wi_accepted = $accept->apply( $wi, $c );
+
+		$bug->workflow_instance( $wi_accepted );
+	}
+
+And the transition has access to our C<$c> object, which references the current
+user.
+
+=head1 IMPLEMENTING RESTRICTIONS
+
+In order to implement the restrictions we specified above we need to know who
+the submitter and owner of the item are.
+
+For this we create our own instance class as well:
+
+	package MyWorkflowInstance;
+	use Moose;
+
+	extends "Class::Workflow::Instance::Simple";
+
+	has owner => (
+		isa => MyUser",
+		is  => "ro", # all instance fields should be read only
+	);
+
+	has submitter => (
+		isa => MyUser",
+		is  => "ro", # all instance fields should be read only
+	);
+
+When the first instance is created the current user is set as the submitter.
+
+Then, as transitions are applied they can check for the restrictions.
+
+This is typically not done in the actual transition body, but rather in
+validation hooks. L<Class::Workflow::Transition::Validate> provides a stanard
+hook, and L<Class::Workflow::Transition::Simple> provides an even easier
+interface for this:
+
+	my $fixed = Class::Workflow::Transition::Simple->new(
+		name          => 'fixed',
+		to_transition => $awaiting_approval,
+		validators    => [
+			sub {
+				my ( $self, $instance, $c ) = @_;
+				die "Not owner" unless $self->instance->owner->id == $c->user->id;
+			},
+		],
+		body => sub {
+			# ...
+		},
+	);
+
+=head1 THIS CLASS
+
+L<Class::Workflow> objects are utility objects to help you create workflows and
+instances without worrying too much about the state and transition objects.
+
+It's usage is overviewed in the L</SYNOPSIS> section.
+
+=head1 FIELDS
+
+=over 4
+
+=item instance_class
+
+=item state_class
+
+=item transition_class
+
+These are the classes to instantiate with.
+
+They default to L<Class::Workflow::Instance::Simple>,
+L<Class::Workflow::State::Simple> and L<Class::Workflow::Transition::Simple>.
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item new_instance
+
+Instantiate the workflow
+
+=item initial_state
+
+Set the starting state of instances.
+
+=item state
+
+=item transition
+
+These two methods create update or retrieve state or transition objects.
+
+They have autovivification semantics for ease of use, and are pretty lax in
+terms of what they accept.
+
+More formal methods are presented below.
+
+They have several forms:
+
+	$wf->state("foo"); # get (and maybe create) a new state with the name "foo"
+
+	$wf->state( foo => $object ); # set $object as the state by the name "foo"
+
+	$wf->state( $object ); # register $object ($object must support the ->name method )
+
+	# create or update the state named "foo" with the following attributes:
+	$wf->state(
+		name       => "foo",
+		validators => [ sub { ... } ],
+	);
+
+	# also works with implicit name:
+	$wf->state( foo =>
+		validators  => [ sub { ... } ],
+	);
+
+(wherever ->state is used ->transition can also be used).
+
+Additionally, whenever you construct a state like this:
+
+	$wf->state(
+		name        => "foo",
+		transitions => [qw/t1 t2/],
+	);
+
+the parameters are preprocessed so that it's as if you called:
+
+	my @transitions = map { $wf->state($_) } qw/t1 t2/;
+	$wf->state(
+		name        => "foo",
+		transitions => [@transitions],
+	);
+
+so you don't have to worry about creating objects first.
+
+=item add_state $name, $object
+
+=item add_transition $name, $object
+
+Explicitly register an object by the name $name.
+
+=item delete_state $name
+
+=item delete_transition $name
+
+Remove an object by the name $name.
+
+Note that this will B<NOT> remove the object from whatever other object reference it, so that:
+
+	$wf->state(
+		name        => "foo",
+		transitions => ["bar"],
+	);
+
+	$wf->delete_transition("bar");
+
+will not remove the object that was created by the name "bar" from the state
+"foo", it's just that the name has been freed.
+
+Use this method with caution.
+
+=item rename_state $old, $new
+
+=item rename_transition $old, $new
+
+Change the name of an object.
+
+=item get_state $name
+
+=item get_transition $name
+
+Get the object by that name or return undef.
+
+=item create_state $name, @args
+
+=item create_transition $name, @args
+
+Call C<construct_state> or C<construct_transition> and then C<add_state> or
+C<add_transition> with the result.
+
+=item construct_state @args
+
+=item construct_transition @args
+
+Call ->new on the appropriate class.
+
+=item expand_attrs \%attrs
+
+This is used by C<create_or_set_state> and C<create_or_set_transition>, and
+will expand the attrs by the names C<to_state>, C<transition> and
+C<transitions> to be objects instead of string names, hash or array references,
+by calling C<autovivify_transitions> or C<autovivify_states>.
+
+In the future this method might be more aggressive, expanding suspect attrs.
+
+=item autovivify_states @things
+
+=item autovivify_transitions @things
+
+Coerce every element in @things into an object by calling
+C<< $wf->state($thing) >> or C<< $wf->transition($thing) >>.
+
+=item create_or_set_state %attrs
+
+=item create_or_set_transition %attrs
+
+If the object by the name $attrs{name} exists, update it's attrs, otherwise
+create a new one.
+
+=back
+
+=head1 SEE ALSO
+
+L<Workflow> - Chris Winters' take on workflows - it wasn't simple enough for me
+(factoring out the XML/factory stuff was difficult and I needed a much more
+dynamic system).
+
+L<http://is.tm.tue.nl/research/patterns/> - lots of explanation and lovely
+flash animations.
 
 =cut
 
