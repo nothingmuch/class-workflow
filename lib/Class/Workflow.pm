2ef8e9205c61eb72cf32b6f32f06ec62bc540b3f
Yuval Kogman <nothingmuch@woobling.org>
2008-01-04 10:34:23 +0000

    copyright for 2008

--- lib/Class/Workflow.pm
+++ lib/Class/Workflow.pm
@@ -807,7 +807,7 @@ Yuval Kogman <nothingmuch@woobling.org>
 
 =head1 COPYRIGHT & LICENSE
 
-	Copyright (c) 2006 Infinity Interactive, Yuval Kogman. All rights
+	Copyright (c) 2006-2008 Infinity Interactive, Yuval Kogman. All rights
 	reserved. This program is free software; you can redistribute
 	it and/or modify it under the same terms as Perl itself.
 
